5b3878ef1d6b7d19be933c3cc0c62b514febc6ba
Pablo Barrera <pabloandresbarrera0@gmail.com>
2024-03-28 00:15:54 -0500

    repositorios con funciones adicionales completados

--- src/main/java/edu/unimagdalena/tiendaEnLinea/repository/ItemPedidoRepository.java
+++ src/main/java/edu/unimagdalena/tiendaEnLinea/repository/ItemPedidoRepository.java
@@ -4,11 +4,13 @@ import edu.unimagdalena.tiendaEnLinea.entity.ItemPedido;
 import edu.unimagdalena.tiendaEnLinea.entity.Pedido;
 import edu.unimagdalena.tiendaEnLinea.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ItemPedidoRepository extends JpaRepository<ItemPedido,Long> {
     List<ItemPedido> findByPedidoId(Pedido pedidoId);
     List<ItemPedido> findByProductoId(Product productoId);
-
+    @Query("SELECT SUM(i.cantidad) FROM ItemPedido i WHERE i.productoId=:producto")
+    Integer totalVentasDeProducto(Product producto);
 }
