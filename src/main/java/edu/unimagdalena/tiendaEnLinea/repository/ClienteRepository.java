3c887ceadcf99a5e86f4c5e228abc8e5e36cf130
Pablo Barrera <pabloandresbarrera0@gmail.com>
2024-03-29 00:47:59 -0500

    repositorio clientes completado

--- src/main/java/edu/unimagdalena/tiendaEnLinea/repository/ClienteRepository.java
+++ src/main/java/edu/unimagdalena/tiendaEnLinea/repository/ClienteRepository.java
@@ -6,10 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ClienteRepository extends JpaRepository<Cliente,Long> {
-    /*
     List<Cliente> findByEmail(String email);
     List<Cliente> findByDireccion(String direccion);
-    List<Cliente> findByNameStartingWith(String nombre);
-
-     */
+    List<Cliente> findByNombreStartingWith(String nombre);
 }
